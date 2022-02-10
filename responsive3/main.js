cdb727a1c876755bcbc163f4c38e51f1236e7616
Lee haemun <lhmun1129@daum.net>
2022-02-10 22:39:24 +0900

    responsive3 연습

---
+++ responsive3/main.js
@@ -0,0 +1,10 @@
+
+  const contentList = document.getElementsByTagName('dt');
+  const contentitems = document.getElementsByTagName('dd')
+  
+    contentList.addEventListener('click', function() {
+    contentitems.style.display = "block";
+    }); 
+
+  
+
