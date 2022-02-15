2f370b80228d6ac4966861f4c7ca4d8e812b1e3f
Lee haemun <lhmun1129@daum.net>
2022-02-15 19:00:00 +0900

    responsive4 완료

--- responsive4/main4.js
+++ responsive4/main4.js
@@ -1,26 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toggleBtn = document.querySelector('.main__share--btn');
   const toggleMenu = document.querySelector('.main__share--menu');
-  const path = document.querySelector('.main__share--btn').childNodes;
+  const image = document.querySelector('.main__image');
+  const name = document.querySelector('.main__name');
+  const path = document.querySelector('.main__share--btn > svg').childNodes;
   
-  toggleBtn.addEventListener('click', function(e){
-    show();
-    // path.classList.add('active');
+  toggleBtn.addEventListener('click', function(){
+    toggleElements();
+  })
+  
+  function toggleElements() {
+    toggleBtn.classList.toggle('active');
+    toggleMenu.classList.toggle('active');
+    path[0].classList.toggle('active');
+    image.classList.toggle('active');
+    name.classList.toggle('active');
+  }
+
+  window.addEventListener('resize', function(){
+    if(window.innerWidth > 768) {
+      offElements();
+    } 
   })
 
-  window.addEventListener('resize', () => {
-    if(window.innerWidth > 1024) {
-      off();
-    }
-  })
-  
-  function show() {
-    toggleBtn.classList.add('active');
-    toggleMenu.style.display = 'flex';
-  }
-  function off() {
-    // toggleMenu.style.display = none;
+  function offElements() {
     toggleBtn.classList.remove('active');
+    toggleMenu.classList.remove('active');
+    path[0].classList.remove('active');
+    
   }
-
+  
 })
